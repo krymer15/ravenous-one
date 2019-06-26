2132216fd9edd5c83d5963b5433a69df5e6444dc
krymer15 <18199459+krymer15@users.noreply.github.com>
2019-06-25 23:11:16 -0400

    Ravenous Final Commit for Codecademy

--- src/App.js
+++ src/App.js
@@ -9,10 +9,19 @@ import SearchBar from './components/SearchBar/SearchBar';
 
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
   
   searchYelp (term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses})
+    });
   }
   
   render () {
@@ -20,7 +29,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     )
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -18,7 +18,7 @@ const Yelp = {
                         city: business.location.city,
                         state: business.location.state,
                         zipCode: business.location.zip_code,
-                        category: business.categories,
+                        category: business.categories[0].title,
                         rating: business.rating,
                         reviewCount: business.review_count 
                     }
